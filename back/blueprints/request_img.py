3334686400511a760e2a9b02e2d3e5f6484e8d8d
fabiologe <fabioqu@tutanota.com>
2024-04-07 20:29:38 +0200

    modified:   back/blueprints/request_img.py

--- back/blueprints/request_img.py
+++ back/blueprints/request_img.py
@@ -4,10 +4,13 @@ from flask import Blueprint, send_from_directory, request, jsonify
 
 send_img_bp = Blueprint("send_img", __name__)
 
-@send_img_bp.route('/send_img/<path:directory>', methods=['POST'])
-def send_img(directory):
-    data = request.get_json()
-    filename = data.get('filename')
-    images_directory = 'img'
+@send_img_bp.route('/send_img', methods=['POST'])
+def send_img():
+    try:
+        data = request.get_json()
+        filename = data.get('filename')
+        images_directory = 'img'
+    except Exception as e:
+        print(f"ERROR YOU DUMBASS!!!: {str(e)}")
     
     return send_from_directory(images_directory, filename)
